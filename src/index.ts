67a1731c0b99d78fe251f855247d16e04868d383
stanvlv <114359328+stanvlv@users.noreply.github.com>
2023-12-27 14:17:41 +0100

    m

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {productsRouter} from './routes/products';
 import {errorHandler} from './middlewares/errorHandler';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
